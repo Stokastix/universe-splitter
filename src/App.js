4fa3af339ebf70accc7f1dc4afc8f2d56dac1365
Louis Faucon <lpfaucon@gmail.com>
2019-11-13 19:07:16 +0100

    Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Split from "./Split";
+import Chart from "./Chart";
+import Stats from "./Stats";
 import "./App.css";
 
 function App() {
+  const [view, setView] = useState("split");
   return (
     <div className="App">
-      <input type="text" defaultValue="Take a Chance" />
-      <input type="text" defaultValue="Play it Safe" />
-      <button>Split</button>
+      {view === "split" && <Split />}
+      {view === "chart" && <Chart />}
+      {view === "stats" && <Stats />}
+      <div id="bottom-nav">
+        <button onClick={() => setView("split")}>Split</button>
+        <button onClick={() => setView("chart")}>Chart</button>
+        <button onClick={() => setView("stats")}>Stats</button>
+      </div>
     </div>
   );
 }
---
+++ src/Split.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <input type="text" defaultValue="Take a Chance" />
+    <input type="text" defaultValue="Play it Safe" />
+    <button>Split</button>
+  </div>
+);
