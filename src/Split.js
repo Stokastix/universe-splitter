6cb89ea71bc051c5ff707724977421c641008b6b
Louis Faucon <lpfaucon@gmail.com>
2019-11-15 16:56:12 +0100

    React states for input fields

--- src/Split.js
+++ src/Split.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import bStorage from "./Backend";
 
-function splitIt() {
+function splitIt(x, y) {
+  console.log(x, y);
   let choice = Math.floor(Math.random() * 2);
   bStorage.setSplit("XXX", "ZZZ", choice);
 }
 
 export default () => {
+  const [inputA, setInputA] = useState("Take a Chance");
+  const [inputB, setInputB] = useState("Play it Safe");
+
   return (
     <div id="splitter-root">
       <h1>Universe Splitter</h1>
@@ -14,11 +18,19 @@ export default () => {
       <h2>Universe Bifurcation</h2>
 
       <p>In one universe I will:</p>
-      <input type="text" defaultValue="Take a Chance" />
+      <input
+        type="text"
+        defaultValue={inputA}
+        onChange={e => setInputA(e.target.value)}
+      />
       <p>In another I will:</p>
-      <input type="text" defaultValue="Play it Safe" />
+      <input
+        type="text"
+        defaultValue={inputB}
+        onChange={e => setInputB(e.target.value)}
+      />
 
-      <span className="split-button" onClick={() => splitIt()}>
+      <span className="split-button" onClick={() => splitIt(inputA, inputB)}>
         Split
       </span>
     </div>
