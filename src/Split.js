468b4c1abb853004a3c4bf9ed319f7f15f8bf8a8
Louis Faucon <lpfaucon@gmail.com>
2019-11-15 15:45:31 +0100

    Changes to Split component

--- src/Split.js
+++ src/Split.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => (
-  <div>
-    <input type="text" defaultValue="Take a Chance" />
-    <input type="text" defaultValue="Play it Safe" />
-    <button>Split</button>
-  </div>
-);
+export default () => {
+  return (
+    <div id="splitter-root">
+      <h1>Universe Splitter</h1>
+      <h2>Quantum-Induce</h2>
+      <h2>Universe Bifurcation</h2>
+
+      <p>In one universe I will:</p>
+      <input type="text" defaultValue="Take a Chance" />
+      <p>In another I will:</p>
+      <input type="text" defaultValue="Play it Safe" />
+      <span className="split-button">Split</span>
+    </div>
+  );
+};
