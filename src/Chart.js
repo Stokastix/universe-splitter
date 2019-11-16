73d8c2f26907a473bbc2d40c23a6e55ea8a33b81
antoinexp <antoinexp@gmail.com>
2019-11-16 13:09:52 +0100

    subcomponents for chart

--- src/Chart.js
+++ src/Chart.js
@@ -10,45 +10,57 @@ class Chart extends Component {
     componentDidMount() {
     }
   
+
     render(){
+      const Outcome = props =>
+        <text height="1" style={{fontSize: "0.8px"}} dominantBaseline="baseline" textAnchor="middle" fill="green" {...props} />;
+
+      const Dot = props => 
+        <circle r="0.3" fill="url(#circleGradient)" filter="url(#f1)" {...props} />;
+
+      const Branch = props => 
+        <line stroke="green" strokeWidth="0.2" {...props} />;
+
+      const Jonction = props => 
+        <g>
+          <Branch x1={props.x} y1={props.y} x2={props.x} y2={props.y+1} />
+          <Branch x1={props.x} y1={props.y+1} x2={props.x-1} y2={props.y+2} />
+          <Branch x1={props.x} y1={props.y+1} x2={props.x+1} y2={props.y+2} />
+          <Dot cx={props.x}  cy={props.y+1} />
+          <Dot cx={props.x}  cy={props.y} />
+        </g>;
+
+
       return (
         <svg width="100%" height="100%">
+          <defs>
+            <radialGradient id="circleGradient">
+              <stop offset="10%" stopColor="gold"/>
+              <stop offset="95%" stopColor="green"/>
+            </radialGradient>
+            <filter id="f1">
+              <feGaussianBlur in="SourceGraphic" stdDeviation="0.1" />
+            </filter>
+          </defs>
+
+
           <rect width="100%" height="100%" fill="black"/>
-          <g style={{transform: 'translate(50%, 50%) scale(20,20) '}}>
-            <text x="0" y="0" height="1" style={{fontSize: "0.8px"}} dominantBaseline="baseline" textAnchor="middle" fill="green">Root Node</text>    
           
-            <g>
-              <line x1="0" y1="1" x2="0" y2="2" stroke="green" strokeWidth="0.2" />
+          <g style={{transform: 'translate(50%, 50%) scale(20,20) '}}>
+            <Outcome x="0" y="0">Root Node</Outcome>    
+          
+            <Jonction x={0} y={1} />
+            <Outcome x="1" y="3" textAnchor="start">Outcome 1 </Outcome>
+            <Outcome x="-1" y="3" textAnchor="end">Outcome 2 </Outcome>
 
-              <line x1="0" y1="2" x2="1" y2="3" stroke="green" strokeWidth="0.2" />
-              <text x="1" y="3" height="1" style={{fontSize: "0.8px"}}  dominantBaseline="baseline" textAnchor="start" fill="green">Outcome 1</text>
+            <Jonction x={1} y={3} />
+            <Outcome x="2" y="5" textAnchor="start">Outcome 1</Outcome>
+            <Outcome x="0" y="5" textAnchor="end">Outcome 1</Outcome>
 
-              <line x1="0" y1="2" x2="-1" y2="3" stroke="green" strokeWidth="0.2" />
-              <text x="-1" y="3" height="1" style={{fontSize: "0.8px"}} dominantBaseline="baseline" textAnchor="end" fill="green">Outcome 2</text>
-
-              <g>
-                <line x1="1" y1="3" x2="1" y2="4" stroke="green" strokeWidth="0.2" />
-
-                <line x1="1" y1="4" x2="2" y2="5" stroke="green" strokeWidth="0.2" />
-                <text x="2" y="5" height="1" style={{fontSize: "0.8px"}}  dominantBaseline="baseline" textAnchor="start" fill="green">Outcome 1</text>
-
-                <line x1="1" y1="4" x2="0" y2="5" stroke="green" strokeWidth="0.2" />
-                <text x="0" y="5" height="1" style={{fontSize: "0.8px"}} dominantBaseline="baseline" textAnchor="end" fill="green">Outcome 2</text>
-
-                <g>
-                  <line x1="0" y1="5" x2="0" y2="6" stroke="green" strokeWidth="0.2" />
-
-                  <line x1="0" y1="6" x2="1" y2="7" stroke="green" strokeWidth="0.2" />
-                  <text x="1" y="7" height="1" style={{fontSize: "0.8px"}}  dominantBaseline="baseline" textAnchor="start" fill="green">Outcome 1</text>
-
-                  <line x1="0" y1="6" x2="-1" y2="7" stroke="green" strokeWidth="0.2" />
-                  <text x="-1" y="7" height="1" style={{fontSize: "0.8px"}} dominantBaseline="baseline" textAnchor="end" fill="green">Outcome 2</text>
-
-                </g>
-
-              </g>
-
-            </g>
+            <Jonction x={0} y={5} />
+            <Outcome x="1" y="7" textAnchor="start">Outcome 1</Outcome>
+            <Outcome x="-1" y="7" textAnchor="end">Outcome 1</Outcome>
+            
 
           </g>
       </svg>
