cf0d14ed623f4260f1f8670fefe3baffd393e88b
Louis Faucon <lpfaucon@gmail.com>
2019-11-17 12:40:38 +0100

    Improve Split component

--- src/Split.js
+++ src/Split.js
@@ -1,38 +1,60 @@
 import React, { useState } from "react";
 import bStorage from "./Backend";
 
-function splitIt(x, y) {
-  console.log(x, y);
-  let choice = Math.floor(Math.random() * 2);
-  bStorage.setSplit(x, y, choice);
-}
+const Splitter = ({ setSplit }) => {
+  const [inputA, setInputA] = useState("");
+  const [inputB, setInputB] = useState("");
+
+  const splitIt = (x, y) => {
+    if (!x || !y) return;
+    const choice = Math.floor(Math.random() * 2);
+    setSplit(choice > 0.5 ? x : y);
+    bStorage.setSplit(x, y, choice);
+  };
+
+  return (
+    <>
+      <p>In one universe I will:</p>
+      <input
+        type="text"
+        placeholder={"Take a Chance"}
+        onChange={e => setInputA(e.target.value)}
+      />
+      <p>In another I will:</p>
+      <input
+        type="text"
+        placeholder={"Play it Safe"}
+        onChange={e => setInputB(e.target.value)}
+      />
+
+      {inputA && inputB && (
+        <span className="split-button" onClick={() => splitIt(inputA, inputB)}>
+          SPLIT
+        </span>
+      )}
+    </>
+  );
+};
+
+const ShowSplit = ({ split }) => {
+  return (
+    <>
+      <p>Now you should:</p>
+      <span className="show-split">{split}</span>
+    </>
+  );
+};
 
 export default () => {
-  const [inputA, setInputA] = useState("Take a Chance");
-  const [inputB, setInputB] = useState("Play it Safe");
+  const [split, setSplit] = useState(undefined);
 
   return (
     <div id="splitter-root">
       <h1>Universe Splitter</h1>
       <h2>Quantum-Induce</h2>
       <h2>Universe Bifurcation</h2>
-
-      <p>In one universe I will:</p>
-      <input
-        type="text"
-        defaultValue={inputA}
-        onChange={e => setInputA(e.target.value)}
-      />
-      <p>In another I will:</p>
-      <input
-        type="text"
-        defaultValue={inputB}
-        onChange={e => setInputB(e.target.value)}
-      />
-
-      <span className="split-button" onClick={() => splitIt(inputA, inputB)}>
-        Split
-      </span>
+      {!split && <Splitter setSplit={setSplit} />}
+      {split && <ShowSplit split={split} />}
     </div>
   );
 };
