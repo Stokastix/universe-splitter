7ef82fefc808e45973c9df12777eacc614f71c94
lfaucon <lpfaucon@gmail.com>
2019-11-19 20:56:56 +0100

    UI - Stats component

--- src/Stats.js
+++ src/Stats.js
@@ -1,3 +1,20 @@
 import React from "react";
 
-export default () => <div>Stats component</div>;
+const pretty = n => {
+  console.log(n);
+  if (n < 10000) return n;
+  const a = n.toExponential(2).split("e+");
+  console.log(a);
+  return a[0] + "·10^" + a[1];
+};
+
+export default () => {
+  return (
+    <div id="stats-root">
+      <h2>THE UNIVERSE</h2>
+      <h2>HAS BEEN SPLIT</h2>
+      <h1>{pretty(142)}</h1>
+      <h2>TIMES</h2>
+    </div>
+  );
+};
